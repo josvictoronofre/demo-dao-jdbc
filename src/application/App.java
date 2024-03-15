72e06497b016cfa49d01a71cb35d7df085b71674
Victor <josvictoronofre@gmail.com>
2024-03-15 09:03:03 -0300

    Reusing instanciation

--- src/application/App.java
+++ src/application/App.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model_dao.DaoFactory;
 import model_dao.SellerDao;
-import model_entities.Department;
 import model_entities.Seller;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
+
+        System.out.println("-- Test 1 - Seller find by ID --");
+
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
