fd6d09bb51bf056766f07028b367882ea2dbac48
Victor <josvictoronofre@gmail.com>
2024-03-15 10:04:25 -0300

    Implementing Seller findAll

--- src/application/App.java
+++ src/application/App.java
@@ -24,5 +24,12 @@ public class App {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("-- Test 3 - Seller findAll --");
+        list = sellerDao.findAll();
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
